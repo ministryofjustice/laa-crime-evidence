109dcbfd95644875ea83ba1374c508b61941383a
Venkat Velpuri <venkat.velpuri@digital.justice.gov.uk>
2023-02-23 16:48:08 +0000

    LCAM-464|Initial repo setup

---
+++ crime-evidence/src/main/java/uk/gov/justice/laa/crime/evidence/CrimeEvidenceApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.justice.laa.crime.evidence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrimeEvidenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrimeEvidenceApplication.class, args);
+    }
+
+}
---
+++ crime-evidence/src/test/java/uk/gov/justice/laa/crime/evidence/CrimeEvidenceApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gov.justice.laa.crime.evidence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrimeEvidenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
