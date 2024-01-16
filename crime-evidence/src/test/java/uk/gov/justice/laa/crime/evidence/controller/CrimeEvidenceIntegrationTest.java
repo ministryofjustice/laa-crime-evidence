04ad4c3fee62f433842e8590c2ca124946a91d49
Sriram Kasthuri <sriram.kasthuri@digital.justice.gov.uk>
2024-01-16 12:25:23 +0000

    LCAM-1148: Upgrade to crime-commons rest-client fat-jar lib and gradle dependency cleanup

--- crime-evidence/src/test/java/uk/gov/justice/laa/crime/evidence/controller/CrimeEvidenceIntegrationTest.java
+++ crime-evidence/src/test/java/uk/gov/justice/laa/crime/evidence/controller/CrimeEvidenceIntegrationTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
@@ -28,21 +29,25 @@ import java.util.UUID;
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DirtiesContext
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Import(CrimeEvidenceTestConfiguration.class)
 @SpringBootTest(classes = CrimeEvidenceApplication.class, webEnvironment = DEFINED_PORT)
+@AutoConfigureWireMock(port = 9999)
 @AutoConfigureObservability
 class CrimeEvidenceIntegrationTest {
 
     private static final String CCP_ENDPOINT_URL = "/api/internal/v1/evidence";
-    private static final String ERROR_MSG = "Call to service MAAT-API failed.";
+    private static final String ERROR_MSG = "Call to service failed. Retries exhausted: 2/2.";
     private static final String CALCULATE_EVIDENCE_FEE = CCP_ENDPOINT_URL.concat("/calculate-evidence-fee");
 
     private MockMvc mvc;
-    private static final WireMockServer wiremock = new WireMockServer(9999);
+
+    @Autowired
+    private WireMockServer wiremock;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -53,21 +58,16 @@ class CrimeEvidenceIntegrationTest {
     @Autowired
     private WebApplicationContext webApplicationContext;
 
-
     @AfterEach
     void after() {
         wiremock.resetAll();
-    }
-
-    @AfterAll
-    static void clean() {
-        wiremock.shutdown();
     }
 
     @BeforeAll
     void startWiremock() {
         wiremock.start();
     }
+
     @BeforeEach
     public void setup() throws JsonProcessingException {
         stubForOAuth();
