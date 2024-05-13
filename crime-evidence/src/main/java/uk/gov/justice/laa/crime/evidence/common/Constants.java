25e82e64fcc0f3aacbec2a7b3f53d1f009b75977
loganathanMOJ <110454701+loganathanMOJ@users.noreply.github.com>
2024-05-13 12:02:06 +0100

    LCAM-1340-add param

--- crime-evidence/src/main/java/uk/gov/justice/laa/crime/evidence/common/Constants.java
+++ crime-evidence/src/main/java/uk/gov/justice/laa/crime/evidence/common/Constants.java
@@ -6,6 +6,10 @@ public class Constants {
     public static final String SENT_FOR_TRIAL = "SENT FOR TRIAL";
     public static final String COMMITTED_FOR_TRIAL = "COMMITTED FOR TRIAL";
 
+    public static final Integer FIRST_REMINDER_DAYS_DUE = 24;
+
+    public static final Integer SECOND_REMINDER_DAYS_DUE = 7;
+
     private Constants() {
         throw new IllegalStateException("Constants class");
     }
