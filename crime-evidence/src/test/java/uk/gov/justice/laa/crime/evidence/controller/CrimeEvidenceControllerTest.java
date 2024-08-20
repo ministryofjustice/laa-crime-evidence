6621a8431ae7b68a9a3c9a6c580826ef123475bf
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2024-08-20 17:08:47 +0100

    Update CrimeEvidenceControllerTest to use @WebMvcTest
    This should the tests efficiency as Spring will not need to spin up the full application context and reduces the configuration required.

--- crime-evidence/src/test/java/uk/gov/justice/laa/crime/evidence/controller/CrimeEvidenceControllerTest.java
+++ crime-evidence/src/test/java/uk/gov/justice/laa/crime/evidence/controller/CrimeEvidenceControllerTest.java
@@ -1,23 +1,16 @@
 package uk.gov.justice.laa.crime.evidence.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.security.web.FilterChainProxy;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 import uk.gov.justice.laa.crime.commons.tracing.TraceIdHandler;
-import uk.gov.justice.laa.crime.evidence.CrimeEvidenceApplication;
-import uk.gov.justice.laa.crime.evidence.config.CrimeEvidenceTestConfiguration;
 import uk.gov.justice.laa.crime.evidence.data.builder.TestModelDataBuilder;
 import uk.gov.justice.laa.crime.evidence.dto.CrimeEvidenceDTO;
 import uk.gov.justice.laa.crime.evidence.service.EvidenceService;
@@ -26,26 +19,17 @@ import uk.gov.justice.laa.crime.util.RequestBuilderUtils;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@DirtiesContext
-@Import(CrimeEvidenceTestConfiguration.class)
-@SpringBootTest(classes = {CrimeEvidenceApplication.class}, webEnvironment = DEFINED_PORT)
+@WebMvcTest(CrimeEvidenceController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class CrimeEvidenceControllerTest {
 
-    private static final boolean IS_VALID = true;
     private static final String ENDPOINT_URL = "/api/internal/v1/evidence/calculate-evidence-fee";
 
-
+    @Autowired
     private MockMvc mvc;
-
-    @Autowired
-    private WebApplicationContext webApplicationContext;
-
-    @Autowired
-    private FilterChainProxy springSecurityFilterChain;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -55,12 +39,6 @@ class CrimeEvidenceControllerTest {
 
     @MockBean
     private TraceIdHandler traceIdHandler;
-
-    @BeforeEach
-    public void setup() {
-        this.mvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext)
-                .addFilter(springSecurityFilterChain).build();
-    }
 
     @Test
     void calculateEvidenceFee_ServerError_RequestBodyIsMissing() throws Exception {
@@ -90,7 +68,7 @@ class CrimeEvidenceControllerTest {
     @Test
     void calculateEvidenceFee_Success() throws Exception {
         var apiCalculateEvidenceFeeRequest =
-                TestModelDataBuilder.getApiCalculateEvidenceFeeRequest(IS_VALID);
+                TestModelDataBuilder.getApiCalculateEvidenceFeeRequest(true);
         var calculateEvidenceFeeRequestJson = objectMapper.writeValueAsString(apiCalculateEvidenceFeeRequest);
         var calculateEvidenceFeeResponse =
                 TestModelDataBuilder.getApiCalculateEvidenceFeeResponse();
