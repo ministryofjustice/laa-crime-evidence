f875bba3665d2cacb5dc1b322e61f17c9dc646b0
Karl Baker <karl.baker@digital.justice.gov.uk>
2024-10-07 16:22:43 +0100

    Remove method

--- crime-evidence/src/main/java/uk/gov/justice/laa/crime/evidence/service/MaatCourtDataService.java
+++ crime-evidence/src/main/java/uk/gov/justice/laa/crime/evidence/service/MaatCourtDataService.java
@@ -30,8 +30,4 @@ public class MaatCourtDataService {
         log.info(String.format(RESPONSE_STRING, response));
         return response.getHeaders().getContentLength();
     }
-
-    public void updateAssessment() {
-
-    }
 }
