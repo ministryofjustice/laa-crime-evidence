fc96eb9336e432143400f17024260127c830a286
Karl Baker <karl.baker@digital.justice.gov.uk>
2024-10-23 16:16:40 +0100

    Add test coverage for validation

--- crime-evidence/src/test/java/uk/gov/justice/laa/crime/evidence/service/EvidenceServiceTest.java
+++ crime-evidence/src/test/java/uk/gov/justice/laa/crime/evidence/service/EvidenceServiceTest.java
@@ -1,36 +1,34 @@
 package uk.gov.justice.laa.crime.evidence.service;
 
-import java.math.BigDecimal;
-import java.util.Collections;
-import org.assertj.core.api.CollectionAssert;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.assertj.core.api.SoftAssertions;
 import org.assertj.core.api.junit.jupiter.InjectSoftAssertions;
 import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import uk.gov.justice.laa.crime.common.model.evidence.ApiIncomeEvidenceItems;
-import uk.gov.justice.laa.crime.common.model.evidence.ApiUpdateIncomeEvidenceRequest;
-import uk.gov.justice.laa.crime.common.model.evidence.ApiUpdateIncomeEvidenceResponse;
+import uk.gov.justice.laa.crime.common.model.evidence.ApiIncomeEvidence;
 import uk.gov.justice.laa.crime.common.model.meansassessment.ApiGetMeansAssessmentResponse;
-import uk.gov.justice.laa.crime.enums.EmploymentStatus;
-import uk.gov.justice.laa.crime.enums.MagCourtOutcome;
-import uk.gov.justice.laa.crime.evidence.builder.UpdateEvidenceDTOBuilder;
+import uk.gov.justice.laa.crime.common.model.meansassessment.ApiIncomeEvidenceSummary;
+import uk.gov.justice.laa.crime.enums.evidence.IncomeEvidenceType;
 import uk.gov.justice.laa.crime.evidence.common.Constants;
 import uk.gov.justice.laa.crime.evidence.data.builder.TestModelDataBuilder;
 import uk.gov.justice.laa.crime.evidence.dto.CrimeEvidenceDTO;
 import uk.gov.justice.laa.crime.common.model.evidence.ApiCalculateEvidenceFeeResponse;
 import uk.gov.justice.laa.crime.enums.EvidenceFeeLevel;
-import uk.gov.justice.laa.crime.evidence.dto.EvidenceReceivedResultDTO;
 import uk.gov.justice.laa.crime.evidence.dto.UpdateEvidenceDTO;
-import uk.gov.justice.laa.crime.evidence.staticdata.enums.ApplicantType;
+import uk.gov.justice.laa.crime.util.DateUtil;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -48,6 +46,9 @@ class EvidenceServiceTest {
 
     @Mock
     private IncomeEvidenceService incomeEvidenceService;
+
+    @Mock
+    private IncomeEvidenceValidationService incomeEvidenceValidationService;
 
     @InjectSoftAssertions
     private SoftAssertions softly;
@@ -138,11 +139,62 @@ class EvidenceServiceTest {
     }
 
     @Test
-    void givenNoEvidenceItems_whenUpdateEvidenceIsInvoked_thenIncomeEvidenceIsNotUpdated() {
+    void givenNoEvidenceItems_whenUpdateEvidenceIsInvoked_thenEvidenceIsNotUpdated() {
         UpdateEvidenceDTO updateEvidenceDTO = TestModelDataBuilder.getUpdateEvidenceRequest();
 
         assertThatThrownBy(() -> evidenceService.updateEvidence(updateEvidenceDTO))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage("No income evidence items provided");
+    }
+
+    // givenMinimumApplicantEvidenceItemsNotReceived_whenUpdateEvidenceIsInvoked_thenEvidenceIsNotUpdated
+
+    @Test
+    void givenValidationErrorOnEvidenceReceivedDate_whenUpdateEvidenceIsInvoked_thenEvidenceIsNotUpdated() {
+        UpdateEvidenceDTO updateEvidenceDTO = TestModelDataBuilder.getUpdateEvidenceRequest();
+        updateEvidenceDTO.setApplicationReceivedDate(LocalDate.now());
+        updateEvidenceDTO.setEvidenceReceivedDate(LocalDateTime.now().minusDays(1));
+        updateEvidenceDTO.setApplicantIncomeEvidenceItems(List.of(
+            new ApiIncomeEvidence(1, LocalDate.of(2024, 9, 1), IncomeEvidenceType.ACCOUNTS, false, "Company accounts"))
+        );
+
+        doThrow(IllegalArgumentException.class).when(incomeEvidenceValidationService).checkEvidenceReceivedDate(
+            DateUtil.parseLocalDate(updateEvidenceDTO.getEvidenceReceivedDate()), updateEvidenceDTO.getApplicationReceivedDate());
+
+        assertThatThrownBy(() -> evidenceService.updateEvidence(updateEvidenceDTO))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void givenValidationErrorOnEvidenceDueDate_whenUpdateEvidenceIsInvoked_thenEvidenceIsNotUpdated() {
+        UpdateEvidenceDTO updateEvidenceDTO = TestModelDataBuilder.getUpdateEvidenceRequest();
+        updateEvidenceDTO.setApplicationReceivedDate(LocalDate.now());
+        updateEvidenceDTO.setEvidenceReceivedDate(LocalDateTime.now().minusDays(1));
+        updateEvidenceDTO.setApplicantIncomeEvidenceItems(List.of(
+            new ApiIncomeEvidence(1, LocalDate.of(2024, 9, 1), IncomeEvidenceType.ACCOUNTS, false, "Company accounts"))
+        );
+
+        LocalDateTime evidenceDueDate = LocalDateTime.now().plusMonths(1);
+        LocalDateTime firstReminderDate = LocalDateTime.now().plusDays(7);
+
+        ApiIncomeEvidenceSummary incomeEvidenceSummary = new ApiIncomeEvidenceSummary();
+        incomeEvidenceSummary.setEvidenceDueDate(evidenceDueDate);
+        incomeEvidenceSummary.setFirstReminderDate(firstReminderDate);
+
+        ApiGetMeansAssessmentResponse getMeansAssessmentResponse = new ApiGetMeansAssessmentResponse();
+        getMeansAssessmentResponse.setIncomeEvidenceSummary(incomeEvidenceSummary);
+
+        when(meansAssessmentApiService.find(TestModelDataBuilder.FINANCIAL_ASSESSMENT_ID)).thenReturn(getMeansAssessmentResponse);
+
+        doThrow(IllegalArgumentException.class).when(incomeEvidenceValidationService).checkEvidenceDueDates(
+            null,
+            DateUtil.parseLocalDate(firstReminderDate),
+            null,
+            DateUtil.parseLocalDate(evidenceDueDate));
+
+        assertThatThrownBy(() -> evidenceService.updateEvidence(updateEvidenceDTO))
+            .isInstanceOf(IllegalArgumentException.class);
+
+        verify(meansAssessmentApiService, atLeastOnce()).find(TestModelDataBuilder.FINANCIAL_ASSESSMENT_ID);
     }
 }
