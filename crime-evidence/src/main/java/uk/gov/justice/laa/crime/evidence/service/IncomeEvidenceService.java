5e3bc06de5870b5ed2461e090c6d93c9696bf16b
loganathanMOJ <110454701+loganathanMOJ@users.noreply.github.com>
2025-02-24 09:23:55 +0000

    remove unused import

--- crime-evidence/src/main/java/uk/gov/justice/laa/crime/evidence/service/IncomeEvidenceService.java
+++ crime-evidence/src/main/java/uk/gov/justice/laa/crime/evidence/service/IncomeEvidenceService.java
@@ -24,7 +24,6 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
